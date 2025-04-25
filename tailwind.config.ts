0a06523d33841eb5f6b1d7b8d3c0f3b0a3260e79
lowkeygud <foreducationalonlytbh@gmail.com>
2025-04-25 12:03:46 +0545

    Styles with tailwind v4 and shadcn components

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+// tailwind.config.ts
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
