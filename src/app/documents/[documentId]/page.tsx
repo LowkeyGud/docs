6a05be4884be4220ac2860161ca4639f6b4be4f2
lowkeygud <foreducationalonlytbh@gmail.com>
2025-04-30 22:34:11 +0545

    feat: add margin ruler

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,9 +1,11 @@
 import Editor from "./editor";
+import { Ruler } from "./ruler";
 import { Toolbar } from "./toolbar";
 
 function Document() {
   return (
     <div className="min-h-screen bg-[#fafbfd]">
+      <Ruler />
       <div className="fixed inset-x-0 top-0 z-10 flex flex-col gap-y-2 bg-[#FAFBFD] px-4 pt-2 print:hidden">
         <Toolbar />
       </div>
