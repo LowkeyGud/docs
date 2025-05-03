da570e0571c16d1e8b9d09cf879efe32825d5eda
lowkeygud <foreducationalonlytbh@gmail.com>
2025-05-03 10:32:51 +0545

    feat: add convex with document schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <NuqsAdapter>
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <ConvexClientProvider>
+          <body className={`${inter.className} antialiased`}>{children}</body>
+        </ConvexClientProvider>
       </NuqsAdapter>
     </html>
   );
